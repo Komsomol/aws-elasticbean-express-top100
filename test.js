13c892890e37edea3a4603b3c1846edd72647bd9
Farhad Agzamov <farhad@thisistommy.com>
2019-04-29 18:14:38 +0100

    upadte on look

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const charts = require("billboard-top-100").getChart
+const charts = require("billboard-top-100").getChart;
 
 const billboardChart = () => {
 	return new Promise((resolve, reject) => {
@@ -7,21 +7,22 @@ const billboardChart = () => {
 				resolve({
 					status: "OK",
 					data: chart.songs
-				})
+				});
 			} else {
 				reject({
 					status: "Error",
 					data: error
-				})
+				});
 			}
-		})
-	})
-}
+		});
+	});
+};
 
 billboardChart().then(result => {
 	if (result.status == "OK") {
-		// console.log(result);sd
+		console.log(result.data[26]);
 	} else {
 		// console.log(error)
 	}
-})
+
+});
