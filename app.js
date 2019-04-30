1d5cc8c2b23b4c4f42e81925ca645f0a27dfcdde
Farhad Agzamov <farhad@thisistommy.com>
2019-04-30 10:49:36 +0100

    comments

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require("express")
-const app = express()
-const path = require("path")
-const port = process.env.PORT || 8081
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 8081;
 
-const charts = require("billboard-top-100").getChart
+const charts = require("billboard-top-100").getChart;
 
-app.set("view engine", "pug")
-app.set("views", path.join(__dirname, "views"))
-app.use(express.static(path.join(__dirname, "views")))
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "views")));
 
 // test end point
 app.get("/test", (req, res) => {
 	res.send({
 		status: "Server is up"
-	})
-})
+	});
+});
 
 // serve route
 app.get("/", function(req, res) {
@@ -24,17 +24,17 @@ app.get("/", function(req, res) {
 				title: "Billboard Top 100",
 				message: "Billboard Top 100",
 				songs: result.data
-			})
+			});
 		} else {
-			res.send(`Error ${result.data}`)
+			res.send(`Error ${result.data}`);
 		}
-	})
-})
+	});
+});
 
 // listen server
 app.listen(port, () => {
-	console.log(`Server running on ${port}`)
-})
+	console.log(`Server running on ${port}`);
+});
 
 const billboardChart = () => {
 	return new Promise((resolve, reject) => {
@@ -43,13 +43,13 @@ const billboardChart = () => {
 				resolve({
 					status: "OK",
 					data: chart.songs
-				})
+				});
 			} else {
 				reject({
 					status: "Error",
 					data: error
-				})
+				});
 			}
-		})
-	})
-}
+		});
+	});
+};
